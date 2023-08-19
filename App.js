74d4c1cd1f5fa4ffc1bb475f1abbe654832927b2
Marcus Tonge <marcustonge@hotmail.co.uk>
2023-08-19 17:09:11 +0100

    Built navigator and some basic page components

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './lib/view/home/home';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './lib/routes/homeStack';
 
 export default function App() {
   return (
-    Home()
+    <Navigator />
+
   );
 }
+
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This is a quick mockup of the existing 4in1 Driving App on the app store.
 
 It will implement a Demo homepage
-Plan Progress page
+Plan Progress page (implement variable progress bar)
 Quiz environment w/ some mock questions from a bank stored in the app or from an API if one exists
 
 Additional features:
